07a31ca9e728a251f503cea952f04971b174d0e4
itsjiaming <cjiaming@comp.nus.edu.sg>
2021-12-16 17:58:05 +0800

    updated collection page

--- pages/CollectionPage.js
+++ pages/CollectionPage.js
@@ -7,26 +7,54 @@ import {
   View,
   Image,
   Button,
+  ScrollView
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 const CollectionPage = () => {
-  const backgroundImage = require("../assets/winterBackground.png");
+  const backgroundImage = require("../assets/livingRoomBackground.png");
+  const pandaImage = require("../assets/panda1.png");
+  const frameImage = require("../assets/picture-frame.png");
+  const mysteryImage = require("../assets/mystery.png");
   const styles = StyleSheet.create({
     container: {
       flex: 1,
     },
+    scrollView: {
+      top: 70,
+      marginHorizontal: 20,
+    },
     text: {
-      color: "white",
-      fontSize: 42,
-      lineHeight: 84,
-      fontWeight: "bold",
-      textAlign: "center",
-      backgroundColor: "#000000c0",
+      color: "black",
+      fontSize: 15,
+      lineHeight: 34,
+      //fontWeight: "bold",
+      //textAlign: "center",
+      //backgroundColor: "#000000c0",
+      position: 'absolute',
+      top: 20,
+      left: 10,
     },
     backgroundImage: {
       flex: 1,
       justifyContent: "center",
+    },
+    panda: {
+      width: 110,
+      height: 110,
+    },
+    frame: {
+      width: 160,
+      height: 160,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    framesPerRow: {
+      height: 180,
+      //position: 'absolute',
+      flexDirection: 'row',  // horizontal
+      justifyContent: 'space-evenly', // main axis
+      alignItems: 'center', // secondary axis
     },
   });
   return (
@@ -35,12 +63,104 @@ const CollectionPage = () => {
       <ImageBackground
         source={backgroundImage}
         style={styles.backgroundImage}
-        resizeMode="cover"
-      >
-        <Text style={styles.text}>Collection Page</Text>
+        > 
+       <Text style={styles.text}>View the pandas you have collected so far!</Text>
+      <SafeAreaView style={styles.container}>
+       <ScrollView style={styles.scrollView}>
+       <View style={styles.framesPerRow}>
+        {<ImageBackground 
+          source={frameImage}
+          style={styles.frame}>
+          <View style={styles.panda}>
+            <Image 
+              source={pandaImage}
+              style={styles.panda}
+                />
+          </View>
+        </ImageBackground>}
+        {<ImageBackground 
+          source={frameImage}
+          style={styles.frame}>
+          <View style={styles.panda}>
+            <Image 
+              source={pandaImage}
+              style={styles.panda}
+                />
+          </View>
+        </ImageBackground>}
+      </View>
+      <View style={styles.framesPerRow}>
+        {<ImageBackground 
+          source={frameImage}
+          style={styles.frame}>
+          <View style={styles.panda}>
+            <Image 
+              source={mysteryImage}
+              style={styles.panda}
+                />
+          </View>
+        </ImageBackground>}
+        {<ImageBackground 
+          source={frameImage}
+          style={styles.frame}>
+          <View style={styles.panda}>
+            <Image 
+              source={pandaImage}
+              style={styles.panda}
+                />
+          </View>
+        </ImageBackground>}
+      </View>
+      {<View style={styles.framesPerRow}>
+        {<ImageBackground 
+          source={frameImage}
+          style={styles.frame}>
+          <View style={styles.panda}>
+            <Image 
+              source={pandaImage}
+              style={styles.panda}
+                />
+          </View>
+        </ImageBackground>}
+        {<ImageBackground 
+          source={frameImage}
+          style={styles.frame}>
+          <View style={styles.panda}>
+            <Image 
+              source={mysteryImage}
+              style={styles.panda}
+                />
+          </View>
+        </ImageBackground>}
+      </View>}
+      {<View style={styles.framesPerRow}>
+        {<ImageBackground 
+          source={frameImage}
+          style={styles.frame}>
+          <View style={styles.panda}>
+            <Image 
+              source={mysteryImage}
+              style={styles.panda}
+                />
+          </View>
+        </ImageBackground>}
+        {<ImageBackground 
+          source={frameImage}
+          style={styles.frame}>
+          <View style={styles.panda}>
+            <Image 
+              source={mysteryImage}
+              style={styles.panda}
+                />
+          </View>
+        </ImageBackground>}
+      </View>}
+        </ScrollView>
+      </SafeAreaView>
       </ImageBackground>
     </View>
   );
 };
 
 export default CollectionPage;
+
